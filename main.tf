5982e833d090984cf180cc3618a82def04e82a34
Nikita Kozelko <nikitakozelko@gmail.com>
2024-09-29 11:09:53 +0200

    configure oidc

--- main.tf
+++ main.tf
@@ -27,6 +27,14 @@ terraform {
   }
  }
 
+resource "aws_iam_openid_connect_provider" "github_oidc" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = ["sts.amazonaws.com"]
+
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]  # Thumbprint for GitHub OIDC provider
+}
+
 resource "aws_iam_role" "GithubActionsRole" {
   name = "GithubActionsRole"
 
