32d5d65ca16463bef06003105e4e4b97d1307769
Nikita Kozelko <nikitakozelko@gmail.com>
2024-10-15 21:53:40 +0200

    add internet gateway for public subnets

--- resources.tf
+++ resources.tf
@@ -83,3 +83,34 @@ variable "availability_zones" {
   type        = list(string)
   default     = ["eu-central-1a", "eu-central-1b"]
 }
+
+# Internet Gateway for the public subnet
+resource "aws_internet_gateway" "terraform-lab-igw" {
+  tags = {
+    Name = "terraform-lab-igw"
+  }
+  vpc_id = aws_vpc.terraform-lab-vpc.id
+}
+
+resource "aws_route_table" "public-route-table" {
+  vpc_id = aws_vpc.terraform-lab-vpc.id
+  tags = {
+    Name = "public-terraform-lab-route-table"
+  }
+}
+
+resource "aws_route" "public-internet-igw-route" {
+  route_table_id         = aws_route_table.public-route-table.id
+  gateway_id             = aws_internet_gateway.terraform-lab-igw.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+# Associate the newly created route tables to the subnets
+resource "aws_route_table_association" "public-route-1-association" {
+  route_table_id = aws_route_table.public-route-table.id
+  subnet_id      = aws_subnet.public-subnet-1.id
+}
+resource "aws_route_table_association" "public-route-2-association" {
+  route_table_id = aws_route_table.public-route-table.id
+  subnet_id      = aws_subnet.public-subnet-2.id
+}
