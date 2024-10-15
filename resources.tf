e250f465f7deb5e9ed511dccfe2cd35906ae7736
Nikita Kozelko <nikitakozelko@gmail.com>
2024-10-15 20:24:41 +0200

    temp commented code with errors

--- resources.tf
+++ resources.tf
@@ -1,11 +1,11 @@
-resource "aws_s3_bucket" "rss-task-2-bucket" {
-  bucket = "rss-task-2-bucket"
-  acl    = "private"
-}
+# resource "aws_s3_bucket" "rss-task-2-bucket" {
+#   bucket = "rss-task-2-bucket"
+#   acl    = "private"
+# }
 
-resource "aws_s3_bucket_versioning" "rss-task-2-bucket-versioning" {
-  bucket = aws_s3_bucket.rss-task-2-bucket.bucket
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
+# resource "aws_s3_bucket_versioning" "rss-task-2-bucket-versioning" {
+#   bucket = aws_s3_bucket.rss-task-2-bucket.bucket
+#   versioning_configuration {
+#     status = "Enabled"
+#   }
+# }
