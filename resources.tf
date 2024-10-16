e6813f69ed2699cc4c1c83cceece2e6fce0e3a89
Nikita Kozelko <nikitakozelko@gmail.com>
2024-10-16 20:49:10 +0200

    created ec2 instances to test created network

--- resources.tf
+++ resources.tf
@@ -137,6 +137,14 @@ resource "aws_security_group" "ec2_security_group" {
     cidr_blocks = ["0.0.0.0/0"]  # from everywhere
   }
 
+  # Allow ping
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   # Allow all out traffic
   egress {
     from_port   = 0
@@ -162,7 +170,7 @@ data "aws_ami" "amazon_linux" {
 }
 
 # Create ec2 instance
-resource "aws_instance" "ec2_public_instance" {
+resource "aws_instance" "ec2_public_instance-subnet-1" {
   ami = data.aws_ami.amazon_linux.id
   instance_type = "t2.micro"
 
@@ -174,5 +182,50 @@ resource "aws_instance" "ec2_public_instance" {
 
   tags = {
     Name = "public-ec2-instance"
+  }
+}
+
+resource "aws_instance" "ec2_public_instance-subnet-2" {
+  ami = data.aws_ami.amazon_linux.id
+  instance_type = "t2.micro"
+
+  subnet_id = aws_subnet.public-subnet-2.id
+
+  associate_public_ip_address = true
+
+  security_groups = [aws_security_group.ec2_security_group.id]
+
+  tags = {
+    Name = "public-ec2-instance"
+  }
+}
+
+resource "aws_instance" "ec2_private_instance-subnet-1" {
+  ami = data.aws_ami.amazon_linux.id
+  instance_type = "t2.micro"
+
+  subnet_id = aws_subnet.private-subnet-1.id
+
+  associate_public_ip_address = true
+
+  security_groups = [aws_security_group.ec2_security_group.id]
+
+  tags = {
+    Name = "private-ec2-instance"
+  }
+}
+
+resource "aws_instance" "ec2_private_instance-subnet-2" {
+  ami = data.aws_ami.amazon_linux.id
+  instance_type = "t2.micro"
+
+  subnet_id = aws_subnet.private-subnet-2.id
+
+  associate_public_ip_address = true
+
+  security_groups = [aws_security_group.ec2_security_group.id]
+
+  tags = {
+    Name = "private-ec2-instance"
   }
 }
