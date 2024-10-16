2d765931d98c86e06ef514f88a7a3fd29bd42b9f
Nikita Kozelko <nikitakozelko@gmail.com>
2024-10-16 21:13:48 +0200

    configure nat gateway

--- resources.tf
+++ resources.tf
@@ -115,6 +115,45 @@ resource "aws_route_table_association" "public-route-2-association" {
   subnet_id      = aws_subnet.public-subnet-2.id
 }
 
+# NAT Gateway
+resource "aws_eip" "nat_gateway" {
+  vpc = true
+  associate_with_private_ip = "10.0.0.5"
+  depends_on                = [aws_internet_gateway.terraform-lab-igw]
+}
+
+resource "aws_nat_gateway" "terraform-lab-ngw" {
+  allocation_id = aws_eip.nat_gateway.id
+  subnet_id     = aws_subnet.public-subnet-1.id
+
+  tags = {
+    Name = "terraform-lab-ngw"
+  }
+  depends_on = [aws_eip.nat_gateway]
+}
+resource "aws_route_table" "private-route-table" {
+  vpc_id = aws_vpc.terraform-lab-vpc.id
+  tags = {
+    Name = "private-terraform-lab-route-table"
+  }
+}
+
+resource "aws_route" "nat-ngw-route" {
+  route_table_id         = aws_route_table.private-route-table.id
+  nat_gateway_id         = aws_nat_gateway.terraform-lab-ngw.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route_table_association" "private-route-1-association" {
+  route_table_id = aws_route_table.private-route-table.id
+  subnet_id      = aws_subnet.private-subnet-1.id
+}
+resource "aws_route_table_association" "private-route-2-association" {
+  route_table_id = aws_route_table.private-route-table.id
+  subnet_id      = aws_subnet.private-subnet-2.id
+}
+
+
 # Security group for EC2
 resource "aws_security_group" "ec2_security_group" {
   name        = "ec2-security-group"
