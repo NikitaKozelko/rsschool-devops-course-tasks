25be3a50ae1895dcaba5346bab024cb89e9a1f80
Nikita Kozelko <nikitakozelko@gmail.com>
2024-10-17 22:34:31 +0200

    add aws key for ec2 instances

--- ec2-instances.tf
+++ ec2-instances.tf
@@ -1,3 +1,10 @@
+resource "aws_key_pair" "EC2-instance_key" {
+  key_name = "K8s-EC2-ssh-key"
+  public_key = file("${path.module}/my-aws-key.pub")
+  # store pub key in github secter
+  # public_key = var.ec2-ssh-key
+}
+
 resource "aws_instance" "ec2_public_instance-subnet-1" {
   ami           = data.aws_ami.amazon_linux.id
   instance_type = "t2.micro"
@@ -7,6 +14,8 @@ resource "aws_instance" "ec2_public_instance-subnet-1" {
   associate_public_ip_address = true
 
   security_groups = [aws_security_group.ec2_security_group.id]
+
+  key_name      = aws_key_pair.EC2-instance_key.key_name
 
   tags = {
     Name = "public-ec2-instance"
@@ -21,6 +30,7 @@ resource "aws_instance" "ec2_public_instance-subnet-2" {
 
   associate_public_ip_address = true
 
+  key_name      = aws_key_pair.EC2-instance_key.key_name
   security_groups = [aws_security_group.ec2_security_group.id]
 
   tags = {
